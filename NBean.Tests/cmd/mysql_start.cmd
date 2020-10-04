29cc39d79f4d72d0f8f391fbc80c1b9eeb4e1d02
MiSeeger <miseeger@gmx.de>
2020-10-04 20:14:10 +0200

    edited for batch execution

--- NBean.Tests/cmd/mysql_start.cmd
+++ NBean.Tests/cmd/mysql_start.cmd
@@ -1,1 +1,4 @@
-@ C:\LARAGON\bin\mysql\mysql-5.7.24-winx64\bin\mysqld --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+@echo off
+@echo MySQL Server
+@C:\LARAGON\bin\mysql\mysql-5.7.24-winx64\bin\mysqld --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+exit
--- NBean.Tests/cmd/mysql_stop.cmd
+++ NBean.Tests/cmd/mysql_stop.cmd
@@ -1,1 +1,5 @@
-@ C:\LARAGON\bin\mysql\mysql-5.7.24-winx64\bin\mysqladmin -u root -p shutdown
+@echo off
+@echo ... stopping MySQL Server.
+@C:\LARAGON\bin\mysql\mysql-5.7.24-winx64\bin\mysqladmin -u root -p shutdown
+@echo MySQL Server is stopped.
+exit
